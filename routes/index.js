8d1a460ddde5ceddc2e6e7653375d66ea1c173e9
eprkswu-macbook <eprkswu-macbook@192.168.14.85>
2016-01-20 16:16:06 +0900

    MongoDB TEST 페이지 추

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var client = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* MongoDB TEST Source */
+router.get('/mongo', function(req, res, next){
+	var data = '';
+	client.connect('mongodb://54.249.83.123:27017/newDB', function(err, db){
+		var cursor = db.collection('users').find();
+		cursor.toArray(function(err, items){
+			res.send(items);
+		});
+	});
 });
 
 module.exports = router;
