35845d3aec1b3b5214bd80cf31f69be1592429ed
정규혁 <kyuhuckman@nate.com>
2016-01-25 14:16:42 +0900

    테스트

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var client = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express2' });
 });
 
 /* MongoDB TEST Source */
