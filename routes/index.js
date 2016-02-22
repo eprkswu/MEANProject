ec9b52f116cdea5919a39bc2eb02ba49ae044941
eprkswu-macbook <eprkswu@sbs.co.kr>
2016-02-22 17:55:25 +0900

    페이스북 공유하기 테스트

--- routes/index.js
+++ routes/index.js
@@ -58,4 +58,30 @@ router.get('/face_book_render/:id', function(req, res, next){
 	res.render('facebook_render', {id:id, image_url:image_url});
 });
 
+router.get('/face_book2/:id', function(req, res, next){
+	var id = req.params.id;
+
+	res.render('facebook2', {id:id});
+});
+
+router.get('/face_book_render2/:id', function(req, res, next){
+	res.writeHead(302, {'Location' : 'http://w3.sbs.co.kr/tv/tvsectionMainImg.do?pgmCtg=T&pgmSct=ET&pgmSort=week&div=pc_enter'});
+	res.end();
+	var id = req.params.id;
+	var image_url = "";
+
+	if(id % 3 == 0){
+		image_url = "http://image.bettyvelvet.me/images/thumbnail/htm_20160104103448237065_99_20160104103607_300-thumbnail.JPG";
+	}else if(id % 4 == 0){
+		image_url = "http://image.bettyvelvet.me/images/thumbnail/htm_20160104103448459474_99_20160104103607_300-thumbnail.JPG";
+	}else if(id % 5 == 0){
+		image_url = "http://image.bettyvelvet.me/images/thumbnail/htm_20160104103447789613_99_20160104103607_300-thumbnail.JPG";
+	}else{
+		image_url = "http://image.bettyvelvet.me/images/thumbnail/htm_20160104103447981551_99_20160104103607_300-thumbnail.JPG";
+	}
+
+	res.render('facebook_render', {id:id, image_url:image_url});
+});
+
+
 module.exports = router;
